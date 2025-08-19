47519727882127f3d2b4883df606fbf559c3532e
Manish Rajkumar Arora <manishrajkumararora@gmail.com>
2025-08-19 23:03:31 +0100

    Update StreamLit_CreditModel.py

--- StreamLit_CreditModel.py
+++ StreamLit_CreditModel.py
@@ -94,7 +94,7 @@ if score == 'Altman Z-Score':
                                 'Altman_D': 'MktCap/TL', 'Altman_E':'Sales/TA'}, inplace = True)
     
     Altman_DF_Display = Altman_DF.apply(lambda s: s.apply('{0:.2f}'.format))
-    st.dataframe(Altman_DF_Display.reset_index(drop=True), use_container_width = True)
+    st.dataframe(Altman_DF_Display, use_container_width = True)
     
 elif score == 'Ohlson O-Score':
     Ohlson_DF = Stock_Year[['Ohlson_A', 'Ohlson_B', 'Ohlson_C', 'Ohlson_D',
@@ -106,11 +106,9 @@ elif score == 'Ohlson O-Score':
     Ohlson_DF.rename(columns = {'Ohlson_A':'TA/GNP', 'Ohlson_B':'TL/TA', 'Ohlson_C':'WC/TA', 
                                 'Ohlson_D': 'CL/CA', 'Ohlson_E':'X', 'Ohlson_F':'NI/TA',
                                 'Ohlson_G': 'FFO/TL', 'Ohlson_H':'Y', 'Ohlson_I': 'NI Ratio'}, inplace = True)
-   
-    Ohlson_DF_style = Ohlson_DF.style.format({"TA/GNP":"{:.2e"})
-    Ohlson_DF_Display = Ohlson_DF_style.apply(lambda s: s.apply('{0:.2f}'.format))
     
-    
+    Ohlson_DF_Display = Ohlson_DF.apply(lambda s: s.apply('{0:.2f}'.format))
+    Ohlson_DF_Display["Ohlson_A"] = Ohlson_DF_Display["Ohlson_A"].apply(lambda x: f"{x:.2e}")
     
     st.dataframe(Ohlson_DF_Display, use_container_width = True)    
         
@@ -119,6 +117,4 @@ else:
     
     
     
-
-
 
