f4cbc14279373ede8d6f06ef2425504112eb0b12
Manish Rajkumar Arora <manishrajkumararora@gmail.com>
2025-08-19 21:40:17 +0100

    Add files via upload

--- StreamLit_CreditModel.py
+++ StreamLit_CreditModel.py
@@ -15,15 +15,18 @@ st.write("DataSources: FRED, YahooFinance, FMP.")
 url_CreditModel = "https://raw.githubusercontent.com/man-aro/Credit-Risk-Model/main/Dataset_Relevant_CreditModel.csv"
 url_KMVData = "https://raw.githubusercontent.com/man-aro/Credit-Risk-Model/tree/main/KMV_Merton/"
 
-Credit_Model = pd.read_csv(url_CreditModel)
-
+Credit_Model_Data = pd.read_csv(url_CreditModel)
+Credit_Model_Data.columns
 stock = st.selectbox("Select a Stock*: ", ('AAPL', 'TSLA', 'AMZN', 'MSFT', 'NVDA', 'GOOGL', 'META', 'NFLX', 'JPM', 'V', 'BAC', 'AMD', 'PYPL', 'DIS', 'T', 'PFE', 'COST', 'INTC', 'KO', 'TGT'))
 
 #Data Summary: Based on Selected Stock
+Credit_Score_Data = Credit_Model_Data[Credit_Model_Data['symbol'] == stock]
+Relevant_Years = Credit_Score_Data['Year'].values
+
+stock = st.selectbox("Select a Stock*: ", (Relevant_Years))
 
 
 
 
 
-
-st.write("### Preview of Data")
+st.write("Preview of Data")
